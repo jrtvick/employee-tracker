0b191e0a427b6a44c476de1db6b5c372f0e8e481
Tyler Vick <jrtvick@gmail.com>
2023-06-07 16:18:18 -0400

    added add functions to index as well as exit option

--- db/index.js
+++ db/index.js
@@ -14,6 +14,15 @@ class Queries {
     viewAllEmployees(){
         return this.connection.promise().query("SELECT * FROM employee")
     }
+    addToDepartments(department){
+        return this.connection.promise().query("INSERT INTO department SET ?", department)
+    }
+    addToRoles(role){
+        return this.connection.promise().query("INSERT INTO role SET ?", role)
+    }
+    addToEmployees(employee){
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee)
+    }
 }
 
 module.exports = new Queries(db)
--- index.js
+++ index.js
@@ -1,56 +1,114 @@
 const inquirer = require("inquirer");
 const db = require("./db");
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'action',
-      choices: ['View all departments',
-      'View all roles',
-      'View all employees',
-      'Add a department',
-      'Add a role',
-      'Add an employee',
-      'Update an employee role'
-    ],
-    }
-  ])
-  .then((answers) => {
-    switch (answers.action) {
-      case 'View all departments': 
-        viewAllDepartments();
-        break;
-      case 'View all roles':
-        viewAllRoles();
-        break;
-      case 'View all employees':
-        viewAllEmployees();
-        break;
-    }
-  });
+mainMenu();
+
+function mainMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "action",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.action) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "Add a department":
+          addToDepartments();
+          break;
+        case "Add a role":
+          addToRoles();
+          break;
+        case "Add an employee":
+          addToEmployees();
+          break;
+        case "Exit":
+          process.exit();
+      }
+    });
+}
 
 function viewAllDepartments() {
   db.viewAllDepartments()
     .then(([data]) => {
       console.table(data);
+      mainMenu();
     })
     .catch((err) => console.log(err));
-};
+}
 
 function viewAllRoles() {
   db.viewAllRoles()
     .then(([data]) => {
       console.table(data);
+      mainMenu();
     })
     .catch((err) => console.log(err));
-};
+}
 
 function viewAllEmployees() {
   db.viewAllEmployees()
     .then(([data]) => {
       console.table(data);
+      mainMenu();
     })
     .catch((err) => console.log(err));
-};
+}
+
+function addToDepartments() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the department you wish to add?",
+        name: "name",
+      },
+    ])
+    .then((response) => {
+      // { name: "MyDepartment"}
+      db.addToDepartments(response)
+        .then(() => {
+          console.log(`${response.name} deparment added`);
+          mainMenu();
+        })
+        .catch((err) => console.log(err));
+    });
+}
+
+function addToRoles() {
+  db.addToRoles()
+    .then(([data]) => {
+      console.table(data);
+      mainMenu();
+    })
+    .catch((err) => console.log(err));
+}
+
+function addToEmployees() {
+  db.addToEmployees()
+    .then(([data]) => {
+      console.table(data);
+      mainMenu();
+    })
+    .catch((err) => console.log(err));
+}
